1b369dd44b0c01647fef4e3c1a6c2f9fa185c68e
jura <jurapapunitsky@gmail.com>
2018-10-16 16:12:54 +0300

    testing

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow() {
+    // create brouser window
+    win = new BrowserWindow({
+       width: 600,
+       height: 600,
+       backgroundColor: '#ffffff'
+    });
+
+    win.loadURL(`file://${__dirname}/dist/conlimit/index.html`);
+
+    //// uncoment below to open the DevTools
+    win.webContents.openDevTools();
+
+    // Event when the window os closed
+    win.on('closed', function () {
+        win = null;
+    });
+}
+
+// create window on electron initialization
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+
+    // On macOS specific close process
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    // macOS cpecific close process
+    if (win === null){
+        createWindow();
+    }
+});
